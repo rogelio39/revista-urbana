acad2634e9a8d879d6113782500a4f747e49f8fd
Rogelio Suleta <andresrogesu@gmail.com>
2024-05-15 22:36:19 -0300

    ..

--- frontend/vite-project/src/Components/AddEditNews/AddEditNews.jsx
+++ frontend/vite-project/src/Components/AddEditNews/AddEditNews.jsx
@@ -143,94 +143,83 @@ const AddEditNews = () => {
     return (
         <div className="flex-col gap-5">
             <div className="m-3 rounded bg-red-300 p-6 my-4 flex flex-wraps items-center justify-center">
+                <form ref={formRef} className={!newsCreated ? `shadow-lg rounded p-6 bg-red-100 flex-col items-center` : 'hidden'} onSubmit={handleSubmit}>
 
-
-                <form ref={formRef} className={!newsCreated ? 'shadow-lg rounded p-6 bg-red-100 flex flex-col items-center' : 'hidden'} onSubmit={handleSubmit}>
                     <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 shadow-md bg-red-100 rounded m-3 p-2 flex items-center justify-start gap-5">
                         <label htmlFor="title">Título:</label>
-                        <input className="px-2 py-1 rounded-lg border border-blue-300 focus:ring-1" type="text" id="title" name="title" required />
+                        <input className="px-2 py-1 rounded-lg border border-blue-300 focus-ring-1" type="text" id="title" name="title" required />
                     </div>
 
-                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
-                        <label>Subtítulos:</label> {/* No necesita htmlFor porque se generarán múltiples entradas */}
+                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 hover:shadow-red-md shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
+
+                        <label htmlFor="subtitles">Subtítulos:</label>
                         {subtitles.map((subtitle, index) => (
                             <div key={index}>
-                                <label htmlFor={`subtitle-${index}`}>Subtítulo {index + 1}:</label> {/* Asociando con el id único */}
-                                <input
-                                    className="px-2 py-1 rounded-lg border border-blue-300 focus:ring-1"
-                                    type="text"
-                                    id={`subtitle-${index}`}
-                                    onChange={(event) => handleSubtitleChange(event, index)}
-                                    name={`subtitle-${index}`} // Asegúrate de que el valor se renderice correctamente
-                                    value={subtitle}
-                                />
+                                <input className="px-2 py-1 rounded-lg border border-blue-300 focus-ring-1" type="text" id={`subtitle-${index}`}
+                                    onChange={(event) => handleSubtitleChange(event, index)} name={`subtitle`} />
                             </div>
                         ))}
-                        <button className="hover:bg-blue-500 hover:text-white shadow-md bg-blue-200 p-2 rounded focus:ring-1" type="button" onClick={addSubtitle}>Agregar Subtítulo</button>
+                        <button className="hover:bg-blue-500 hover:text-white shadow-mg bg-blue-200 p-2 rounded focus:ring-1" type="button" onClick={addSubtitle}>Agregar Subtítulo</button>
                     </div>
 
-                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
-                        <label>Categorías:</label> {/* No necesita htmlFor porque se generarán múltiples entradas */}
+                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 hover:shadow-red-md shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
+
+                        <label htmlFor="category">Categorias:</label>
                         {categorys.map((category, index) => (
                             <div key={index}>
-                                <label htmlFor={`category-${index}`}>Categoría {index + 1}:</label> {/* Asociando con el id único */}
-                                <input
-                                    className="px-2 py-1 rounded-lg border border-blue-300 focus:ring-1"
-                                    type="text"
-                                    id={`category-${index}`}
-                                    onChange={(event) => handleCategoryChange(event, index)}
-                                    name={`category-${index}`} // Asegúrate de que el valor se renderice correctamente
-                                    value={category}
-                                />
+                                <input className="px-2 py-1 rounded-lg border border-blue-300 focus-ring-1" type="text" id={`category-${index}`}
+                                    onChange={(event) => handleCategoryChange(event, index)} name={`category`} />
                             </div>
                         ))}
-                        <button className="hover:bg-blue-500 hover:text-white shadow-md bg-blue-200 p-2 rounded focus:ring-1" type="button" onClick={addCategory}>Agregar Categoría</button>
+                        <button className="hover:bg-blue-500 hover:text-white shadow-mg bg-blue-200 p-2 rounded focus:ring-1" type="button" onClick={addCategory}>Agregar Categoria</button>
                     </div>
 
-                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
-                        <select name="font" value={font} onChange={handleChangeFont} className="px-2 py-1 rounded-lg border border-blue-300 focus:ring-1">
-                            <option value="font-sans">Font-sans</option>
+                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 hover:shadow-red-md shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
+
+                        <select name="font" value={font} onChange={handleChangeFont}>
+                            <option value="font-sanzs">Font-sanzs</option>
                             <option value="font-serif">Font-serif</option>
                             <option value="font-mono">Font-mono</option>
                         </select>
                     </div>
 
-                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
-                        <label>Textos:</label> {/* No necesita htmlFor porque se generarán múltiples entradas */}
+                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 hover:shadow-red-md shadow-md bg-red-100 rounded m-3 p-1 flex items-center justify-start gap-5">
+                        <label htmlFor="texts">Textos:</label>
                         {texts.map((text, index) => (
                             <div key={index}>
-                                <label htmlFor={`text-${index}`}>Texto {index + 1}:</label> {/* Asociando con el id único */}
                                 <textarea
-                                    className="whitespace-pre-line px-2 py-1 rounded-lg border border-blue-300 focus:ring-1"
+                                    className="whitespace-pre-line px-2 py-1 rounded-lg border border-blue-300 focus-ring-1"
                                     id={`text-${index}`}
-                                    name={`text-${index}`} // Asegúrate de que el valor se renderice correctamente
+                                    name={`text`}
                                     onChange={(event) => handleTextChange(index, event)}
                                     rows="24"
                                     cols="80"
-                                    value={text}
+                                    value={text} // Renderizar el valor del textarea si es un string simple
                                 />
                             </div>
                         ))}
-                        <button className="hover:bg-blue-500 hover:text-white shadow-md bg-blue-200 p-2 rounded focus:ring-1" type="button" onClick={addText}>Agregar Texto</button>
+                        <button className="hover:bg-blue-500 hover:text-white shadow-mg bg-blue-200 p-2 rounded focus:ring-1" type="button" onClick={addText}>Agregar Texto</button>
                     </div>
 
-                    <button className="hover:bg-blue-500 hover:text-white shadow-md bg-blue-200 p-2 rounded focus:ring-1 m-3 w-24 flex items-center justify-center gap-5" type="submit">
-                        {newsCreated ? 'cargando' : (isCreating ? 'crear' : 'actualizar')}
-                    </button>
+                    {
+                        <button className="hover:bg-blue-500 hover:text-white shadow-mg bg-blue-200 p-2 rounded focus:ring-1 m-3 w-24 flex items-center justify-center gap-5" type="submit">{newsCreated ? 'cargando' : (isCreating ? 'crear' : 'actualizar')}</button>
+                    }
                 </form>
 
-                <div className={newsCreated ? 'shadow-lg rounded p-6 bg-red-100 flex flex-col items-center' : 'hidden'}>NOTICIA CARGADA CORRECTAMENTE</div>
 
+
+
+                
+                <div className={newsCreated ? 'shadow-lg rounded p-6 bg-red-100 flex-col items-center' : 'hidden'}>NOTICA CARGADA CORECTAMENTE</div>
                 <div>
-                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 shadow-md bg-red-100 rounded m-3 p-1 flex flex-col items-center justify-center gap-5">
+
+                    <div className="hover:shadow-xl hover:shadow-red-400 transition-shadow duration-700 hover:shadow-red-md shadow-md bg-red-100 rounded m-3 p-1 flex-col items-center justify-center gap-5">
                         <form className="flex items-center gap-2" encType="multipart/form-data">
                             <label htmlFor="newsImage">Imagen de portada:</label>
                             <input type="file" id="newsImage" name="newsImage" accept="image/*" multiple required />
-                            <button onClick={handleSubmitImage} className="hover:bg-blue-500 hover:text-white shadow-md bg-blue-200 p-2 rounded focus:ring-1" type="button">SUBIR IMAGEN</button>
+                            <button onClick={handleSubmitImage} className="hover:bg-blue-500 hover:text-white shadow-mg bg-blue-200 p-2 rounded focus:ring-1" type="button">SUBIR IMAGEN</button>
                         </form>
                     </div>
-                </div>
-                
                     <div className="flex items-center justify-center">
                         <button className="hover:bg-blue-500 hover:text-white shadow-mg bg-blue-200 p-2 rounded focus:ring-1" onClick={() => setIsCreating(!isCreating)}>
                             {isCreating ? 'Cambiar para editar' : 'Cambiar para crear'}
@@ -241,12 +230,12 @@ const AddEditNews = () => {
             </div>
             <div>
                 {
-                    newsCreated && imageUpload ?
-                        <div className="m-4 p-4">
-                            <h1 className="bg-red-200 text-center font-bold text-xl">Noticia `{lastNew.title}` actualizada correctamente</h1>
-                            <New data={lastNew} />
-                        </div>
-                        : <p className="hidden"></p>},
+                    newsCreated && imageUpload ? 
+                    <div className="m-4 p-4">
+                        <h1 className="bg-red-200 text-center font-bold text-xl">Noticia `{lastNew.title}` actualizada correctamente</h1>
+                        <New data={lastNew} /> 
+                    </div>
+                    : <p className="hidden"></p>},
             </div>
             <div>
                 {
