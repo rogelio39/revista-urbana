96e4a7398e68073fc676f807a9034eb25f3c8fa7
Rogelio Suleta <andresrogesu@gmail.com>
2024-05-20 11:14:11 -0300

    se implemento el menu desplegable

--- frontend/vite-project/src/Components/NavBar/Navbar.jsx
+++ frontend/vite-project/src/Components/NavBar/Navbar.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 
 const Navbar = () => {
     const [searchQuery, setSearchQuery] = useState('')
+    const [menuOpen, setMenuOpen] = useState(false)
+
+
+
+    const handleClickOutside = (e) => {
+        e.preventDefault()
+        if (!e.target.closest('.menu-container')) {
+            setMenuOpen(false)
+        }
+    }
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.addEventListener('click', handleClickOutside);
+        }
+        else {
+            document.removeEventListener('click', handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener('click', handleClickOutside)
+        }
+    }, [menuOpen])
 
     const navigate = useNavigate();
 
@@ -30,41 +52,56 @@ const Navbar = () => {
         navigate('/login')
     }
 
+    const openMenu = () => {
+        setMenuOpen(!menuOpen)
+        console.log(menuOpen)
+    }
 
 
     return (
-        <div className="flex bg- justify-between shadow-xl  p-4">
+        <div className="flex bg- justify-between items-center shadow-xl  p-4">
             <div>
-            <h1 onClick={goToMain} className=" text-red-700 cursor-pointer text-4xl font-bold">REVISTA</h1>
-            <h1 onClick={goToMain} className=" text-blue-800 cursor-pointer text-4xl font-bold">URBANA</h1>
+                <h1 onClick={goToMain} className=" text-red-700 cursor-pointer text-4xl font-bold">REVISTA</h1>
+                <h1 onClick={goToMain} className=" text-blue-800 cursor-pointer text-4xl font-bold">URBANA</h1>
             </div>
-            <ul className="flex items-center gap-5">
-                <li>
-                    <form className="flex gap-2 shadow-md" onSubmit={handleSearch}>
-                        <input 
+            <div>
+                <form className="flex gap-2 shadow-md" onSubmit={handleSearch}>
+                    <input
                         className="p-1 bg-black bg-opacity-90 rounded text-white"
                         type="text"
                         value={searchQuery}
                         placeholder="Buscar noticias"
                         onChange={(e) => setSearchQuery(e.target.value)}
-                        />
+                    />
 
-                        <button className="p-2 shadow-lg bg-black hover:bg-blue-500 text-white rounded" type="submit">Buscar</button>
-                    </form>
-                </li>
-                <li><button onClick={goToLogin} className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">LOGIN</button></li>
-                <li><button onClick={goToHealthy} className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">SALUD</button></li>
-                <li><button onClick={goToPolitics} className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">POLITICA</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">ESPECTACULO</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">DESTACADOS</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">SOCIEDAD</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">CULTURA</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">MUNDO</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">ECONOMIA</button></li>
-                <li><button className="text-white hover:bg-blue-500 rounded p-2 text-1xl  bg-black shadow-lg shrink-0 focus:ring-1">---</button></li>
-            </ul>
+                    <button className="p-2 shadow-lg bg-black hover:bg-blue-500 text-white rounded" type="submit">Buscar</button>
+                </form>
+            </div>
+            <div className="relative menu-container">
+                <div onClick={openMenu} className=" flex flex-col gap-1 cursor-pointer"  aria-expanded={menuOpen} aria-label="Toggle menu">
+                    <span className="w-8 h-1  bg-black"></span>
+                    <span className="w-8 h-1  bg-black"></span>
+                    <span className="w-8 h-1  bg-black"></span>
+                </div>
+                {
+                    menuOpen && (
+                        <ul className="absolute mt-2 w-48 right-0 flex flex-col items-start gap-5 bg-black transition duration-300 ease-in-out">
+                            <li className="w-full hover:bg-blue-500 bg-black"><button onClick={goToLogin} className="block w-full  text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">LOGIN</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button onClick={goToHealthy} className="block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">SALUD</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button onClick={goToPolitics} className="block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">POLITICA</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">ESPECTACULO</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">DESTACADOS</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">SOCIEDAD</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">CULTURA</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">MUNDO</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">ECONOMIA</button></li>
+                            <li className="w-full hover:bg-blue-500 bg-black"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">---</button></li>
+                        </ul>
+                    )
+                }
+            </div>
 
-        </div>
+        </div >
     )
 }
 
