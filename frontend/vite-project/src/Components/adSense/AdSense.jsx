d92dd38e025d7b262b34a64a818cdb3a5f96c21d
Rogelio Suleta <andresrogesu@gmail.com>
2024-06-10 16:51:19 -0300

    cambios en componente adsense

--- frontend/vite-project/src/Components/adSense/AdSense.jsx
+++ frontend/vite-project/src/Components/adSense/AdSense.jsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 const AdSense = () => {
-
     useEffect(() => {
         document.title = "REVISTA URBANA";
         const script = document.createElement("script");
@@ -9,17 +8,31 @@ const AdSense = () => {
         script.async = true;
         script.crossOrigin = "anonymous";
         document.head.appendChild(script);
+        
+        const adsLoader = () => {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        };
+
+        // Esperar hasta que la página se haya cargado completamente
+        if (document.readyState === 'complete') {
+            adsLoader();
+        } else {
+            window.addEventListener('load', adsLoader);
+        }
+
         return () => {
             document.head.removeChild(script);
         };
     }, []);
+
     return (
         <ins className="adsbygoogle block"
             data-ad-client="ca-pub-5685964602459573"
             data-ad-slot="1234567890"
-            data-ad-format="auto"></ins>
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+        ></ins>
+    );
+};
 
-    )
-}
-
-export default AdSense
+export default AdSense;
