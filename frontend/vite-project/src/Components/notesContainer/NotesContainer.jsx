c53d7481c6d377985fdbcbb41b5095a6a4f2b759
Rogelio Suleta <andresrogesu@gmail.com>
2024-06-10 14:46:03 -0300

    error, no se habia quitaod importacion de lazysizes

--- frontend/vite-project/src/Components/notesContainer/NotesContainer.jsx
+++ frontend/vite-project/src/Components/notesContainer/NotesContainer.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
-import 'lazysizes'
+
 
 const NotesContainer = ({ data }) => {
 
@@ -8,7 +8,7 @@ const NotesContainer = ({ data }) => {
         <div className='bg-white rounded flex justify-center items-center'>
             <Link to={`/newById/${data._id}`}>
                 <div className='hover:shadow-white hover:shadow-2xl  shadow-white shadow-md  flex-column h-96 overflow-hidden w-72 justify-between items-center' id={data._id}>
-                    <img className='lazyload rounded mb-5 h-56' data-src={data.thumbnail[0]} alt={data.title} />
+                    <img className='rounded mb-5 h-56' loading='lazy' src={data.thumbnail[0]} alt={data.title} />
                     <h1 className='m-3 mb-5 text-xl font-bold'>{data.title}</h1>
                 </div>
             </Link>
