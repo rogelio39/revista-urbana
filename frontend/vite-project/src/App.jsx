4dedea411516ef5c187b2aad69a590d9645bdd78
Rogelio Suleta <andresrogesu@gmail.com>
2024-06-10 23:06:59 -0300

    ..

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -26,11 +26,11 @@ function App() {
           <AuthProvider>
             <ImageLCP/>
             <Navbar />
+            <AdSense/>
             <Suspense fallback={<div>Cargando...</div>}>
               <Routes>
                 <Route path='*' />
                 <Route path='/' element={<MainSection />} />
-                <AdSense/>
                 <Route path='/add-news' element={<AddEditNews />} />
                 <Route path='/search/:query' element={<SearchResults />} />
                 <Route path='/newsByCategory/:query' element={<NewsByCategory />} />
