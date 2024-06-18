69cb33e1093104c3965a200c256c43233e83e0c5
Rogelio Suleta <andresrogesu@gmail.com>
2024-06-18 16:17:44 -0300

    prueba para agregar google analitycs

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import GoogleAnalytics from './Components/googleAnalytics/GoogleAnalytics.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GoogleAnalytics/>
     <App />
   </React.StrictMode>,
 )
