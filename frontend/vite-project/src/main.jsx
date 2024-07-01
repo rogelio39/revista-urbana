14b9649ed8f0bedf55ff2f78b58c51f99d1a44f8
Rogelio Suleta <andresrogesu@gmail.com>
2024-07-01 14:28:59 -0300

    se agrego google analytics directo en html y tambien  google adsense para no empeorar rendimiento

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import GoogleAnalytics from './Components/googleAnalytics/GoogleAnalytics.jsx'
-// import GoogleAnalytics from './Components/googleAnalytics/GoogleAnalytics.jsx'
-// import AdSense from './Components/adSense/AdSense.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <GoogleAnalytics/>
     <App />
   </React.StrictMode>,
 )
