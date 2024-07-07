a4620fbd747f380b71dc311c2dd946e7b495d656
Rogelio Suleta <andresrogesu@gmail.com>
2024-07-07 16:13:48 -0300

    ..

--- frontend/vite-project/src/Components/NavBar/Navbar.jsx
+++ frontend/vite-project/src/Components/NavBar/Navbar.jsx
@@ -98,6 +98,9 @@ const Navbar = () => {
     const goToRegister = () => {
         navigate('/register')
     }
+    const goToProfile = () => {
+        navigate('/profile')
+    }
 
     const logoWidth = 12
 
@@ -129,9 +132,8 @@ const Navbar = () => {
                         <ul role='z-0 menu'>
                             <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button onClick={goToLogin} className="block w-full  text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">LOGIN</button></li>
                             <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button onClick={goToRegister} className="block w-full  text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">REGISTER</button></li>
-                            <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button onClick={goToHealthy} className="block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">SALUD</button></li>
+                            <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button onClick={goToProfile} className="block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">MI PERFIL</button></li>
                             <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button onClick={goToPolitics} className="block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">POLITICA</button></li>
-                            <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1" onClick={goToShow}>ESPECTACULO</button></li>
                         </ul>
                         <ul role="menu">
                             <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1" onClick={goToSociety}>SOCIEDAD</button></li>
@@ -149,6 +151,8 @@ const Navbar = () => {
                         </ul>
                         <ul role="menu">
                             <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1" onClick={goToNoticiasBandeñas}>NOTICIAS BANDEÑAS</button></li>
+                            <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button className=" block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1" onClick={goToShow}>ESPECTACULO</button></li>
+                            <li role='menu-item' className=" hover:text-slate-800 hover:border-2 hover:border-slate-800 hover:bg-slate-500"><button onClick={goToHealthy} className="block w-full text-white  rounded p-2 text-1xl  shadow-lg shrink-0 focus:ring-1">SALUD</button></li>
                         </ul>
 
                     </div>
